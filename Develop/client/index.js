3d1f76604f1876cfdd3f6dd683d59636ce154878
justusmcdougal317 <justusmcdougal317@gmail.com>
2024-02-14 11:28:50 -0500

    directory structure change

---
+++ Develop/client/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import App from './App';
+
+const client = new ApolloClient({
+  uri: '/graphql', // Update with your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
